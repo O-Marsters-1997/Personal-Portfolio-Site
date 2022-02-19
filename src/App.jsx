4b478e1a0d156113fa97c3475ced7b64a8993869
O-Marsters-1997 <olly.marsters@yahoo.co.uk>
2022-02-19 14:23:50 +0000

    set up theme provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,18 @@
-import React from "react";
+
 import "./styles/css/style.css";
 import { Route, Routes } from "react-router-dom";
-import {
-  ThemeProvider,
-  Button,
-  AppBar,
-  Toolbar,
-  IconButton,
-  Typography,
-} from "@mui/material";
-import MenuIcon from "@mui/icons-material/Menu";
+import theme from "./theme";
+import Navbar from './components/Navbar';
+import { ThemeProvider } from "@mui/material";
 
-const App = () => {
+function App() {
   return (
-    <div className="app">
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
-      <Button>hello</Button>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Navbar></Navbar>
+      </div>
+    </ThemeProvider>
   );
-};
+}
 
 export default App;
---
+++ src/theme.js
@@ -0,0 +1,8 @@
+import { createTheme } from "@mui/material/styles";
+import { purple, orange, red } from "@mui/material/colors";
+
+const theme = createTheme({
+  
+});
+
+export default theme;
